360813374433171cd1813516d88a26535de3b4e8
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-02-28 12:11:12 -0500

    add rest of data to bar chart, add legend, title, y/x axis, center the bars

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 col_count = 3
+bar_width = .1
+alpha = .4
 
 korea_scores = (554, 536, 538)
 canada_scores = (518, 523, 525)
@@ -10,7 +12,17 @@ france_scores = (495, 505, 499)
 
 index = np.arange(col_count)
 
-k1 = plt.bar(index, korea_scores, .5)
+k1 = plt.bar(index, korea_scores, bar_width, alpha=alpha, label="Korea")
+c1 = plt.bar(index + 0.1, canada_scores, bar_width, alpha=alpha, label="Canada")
+ch1 = plt.bar(index + 0.2, china_scores, bar_width, alpha=alpha, label="China")
+f1= plt.bar(index + 0.3, france_scores, bar_width, alpha=alpha, label="France")
+
+plt.ylabel("Mean score in PISA 2012")
+plt.xlabel("Subjects")
+plt.title("Test Scores by country")
+
+plt.xticks(index + .3/2, ("Math", "Reading", "Science"))
+plt.legend()
 plt.grid(True)
 
 plt.show()
