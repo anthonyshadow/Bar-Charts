f6cd71283127a65fef7d95d24cabf7ff2e4795b0
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-02-28 12:23:53 -0500

    create labels for each bar, make bars wider

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 col_count = 3
-bar_width = .1
+bar_width = .2
 alpha = .4
 
 korea_scores = (554, 536, 538)
@@ -13,9 +13,21 @@ france_scores = (495, 505, 499)
 index = np.arange(col_count)
 
 k1 = plt.bar(index, korea_scores, bar_width, alpha=alpha, label="Korea")
-c1 = plt.bar(index + 0.1, canada_scores, bar_width, alpha=alpha, label="Canada")
-ch1 = plt.bar(index + 0.2, china_scores, bar_width, alpha=alpha, label="China")
-f1= plt.bar(index + 0.3, france_scores, bar_width, alpha=alpha, label="France")
+c1 = plt.bar(index + 0.2, canada_scores, bar_width, alpha=alpha, label="Canada")
+ch1 = plt.bar(index + 0.4, china_scores, bar_width, alpha=alpha, label="China")
+f1= plt.bar(index + 0.6, france_scores, bar_width, alpha=alpha, label="France")
+
+def CreateLabels(data):
+    for item in data:
+        height = item.get_height()
+        plt.text(item.get_x() + item.get_width() / 2, height * 1.05,
+        '%d' % int(height), ha='center', va="bottom")
+
+CreateLabels(k1)
+CreateLabels(c1)
+CreateLabels(ch1)
+CreateLabels(f1)
+
 
 plt.ylabel("Mean score in PISA 2012")
 plt.xlabel("Subjects")
@@ -24,5 +36,6 @@ plt.title("Test Scores by country")
 plt.xticks(index + .3/2, ("Math", "Reading", "Science"))
 plt.legend()
 plt.grid(True)
+
 
 plt.show()
